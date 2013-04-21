183f98f3a8c6596676ceb14a0743aab3c2596cd8
Vinod Reddy <kvr2277@gmail.com>
2013-04-21 21:06:24 +0530

    FirstCommit

---
+++ theinventory/src/main/java/svinbass/theinventory/App.java
@@ -0,0 +1,13 @@
+package svinbass.theinventory;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
