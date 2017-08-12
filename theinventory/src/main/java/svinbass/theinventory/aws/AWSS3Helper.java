75206ba94d4d4a46e0a47411cbf50d250eecac71
Vinod Reddy Kisanagaram <kvr2277@gmail.com>
2017-08-12 07:27:21 -0400

    changing sqs messge size

--- theinventory/src/main/java/svinbass/theinventory/aws/AWSS3Helper.java
+++ theinventory/src/main/java/svinbass/theinventory/aws/AWSS3Helper.java
@@ -27,86 +27,76 @@ import com.amazonaws.services.s3.model.PutObjectRequest;
 import com.amazonaws.services.s3.model.S3ObjectSummary;
 
 public class AWSS3Helper {
-	
+
 	private static final String SUFFIX = "/";
 	private static final Logger logger_c = Logger.getLogger(AWSS3Helper.class);
-	
-	
+
 	public static void main(String[] args) {
 		// credentials object identifying user for authentication
-		// user must have AWSConnector and AmazonS3FullAccess for 
+		// user must have AWSConnector and AmazonS3FullAccess for
 		// this example to work
-		AWSCredentials credentials = new BasicAWSCredentials(System.getProperty("AWS_ACCESS_KEY_ID"), System.getProperty("AWS_SECRET_ACCESS_KEY"));
-		
+		AWSCredentials credentials = new BasicAWSCredentials(
+				System.getProperty("AWS_ACCESS_KEY_ID"),
+				System.getProperty("AWS_SECRET_ACCESS_KEY"));
+
 		// create a client connection based on credentials
 		AmazonS3 s3client = new AmazonS3Client(credentials);
-		
+
 		// create bucket - name must be unique for all S3 users
 		String bucketName = "vinzone-example-bucket";
 		s3client.createBucket(bucketName);
-		
+
 		// list buckets
 		for (Bucket bucket : s3client.listBuckets()) {
 			logger_c.info(" - " + bucket.getName());
 		}
-		
+
 		// create folder into bucket
 		String folderName = "testfolder";
 		createFolder(bucketName, folderName, s3client);
-		
+
 		// upload file to folder and set it to public
 		String fileName = folderName + SUFFIX + "VINOD.jpg";
-		s3client.putObject(new PutObjectRequest(bucketName, fileName, 
-				new File("E:\\Goodies\\images\\VIN.jpg"))
+		s3client.putObject(new PutObjectRequest(bucketName, fileName, new File(
+				"E:\\Goodies\\images\\VIN.jpg"))
 				.withCannedAcl(CannedAccessControlList.PublicRead));
-		
-		//deleteFolder(bucketName, folderName, s3client);
-		
+
+		// deleteFolder(bucketName, folderName, s3client);
+
 		// deletes bucket
-		//s3client.deleteBucket(bucketName);
+		// s3client.deleteBucket(bucketName);
 	}
-	
-	public static void putFileInS3(File file)	{
-		
+
+	public static void putFileInS3(File file) {
+
 		logger_c.info("Inside putFileInS3");
-	
-				// location (~/.aws/credentials) using aws configure cli
-				AWSCredentials credentials = null;
-		        try {
-		            credentials = new ProfileCredentialsProvider().getCredentials();
-		        } catch (Exception e) {
-		            throw new AmazonClientException(
-		                    "Cannot load the credentials from the credential profiles file. " +
-		                    "Please make sure that your credentials file is at the correct " +
-		                    "location (~/.aws/credentials), and is in valid format.",
-		                    e);
-		        }
-		        
-				AmazonS3 s3client = AmazonS3ClientBuilder.defaultClient(); 
-				
-				// create bucket - name must be unique for all S3 users
-				String bucketName = "vinzone-example-bucket";
-				s3client.createBucket(bucketName);
-				
-				// list buckets
-				for (Bucket bucket : s3client.listBuckets()) {
-					logger_c.info(" - " + bucket.getName());
-				}
-				
-				// create folder into bucket
-				String folderName = "testfolder1";
-				createFolder(bucketName, folderName, s3client);
-				
-				// upload file to folder and set it to public
-				String fileName = folderName + SUFFIX + file.getName();
-				s3client.putObject(new PutObjectRequest(bucketName, fileName, 
-						file)
-						.withCannedAcl(CannedAccessControlList.PublicRead));
-				
-				logger_c.info("exiting putFileInS3");
-		
+
+		AmazonS3 s3client = AmazonS3ClientBuilder.defaultClient();
+		logger_c.info("After creating  s3client");
+		// create bucket - name must be unique for all S3 users
+		String bucketName = "vinzone-example-bucket";
+		s3client.createBucket(bucketName);
+
+		// list buckets
+		for (Bucket bucket : s3client.listBuckets()) {
+			logger_c.info(" - " + bucket.getName());
+		}
+
+		// create folder into bucket
+		String folderName = "testfolder1";
+		createFolder(bucketName, folderName, s3client);
+
+		// upload file to folder and set it to public
+		String fileName = folderName + SUFFIX + file.getName();
+		s3client.putObject(new PutObjectRequest(bucketName, fileName, file)
+				.withCannedAcl(CannedAccessControlList.PublicRead));
+
+		logger_c.info("exiting putFileInS3");
+
 	}
-	public static void createFolder(String bucketName, String folderName, AmazonS3 client) {
+
+	public static void createFolder(String bucketName, String folderName,
+			AmazonS3 client) {
 		// create meta-data for your folder and set content-length to 0
 		ObjectMetadata metadata = new ObjectMetadata();
 		metadata.setContentLength(0);
@@ -118,13 +108,15 @@ public class AWSS3Helper {
 		// send request to S3 to create folder
 		client.putObject(putObjectRequest);
 	}
+
 	/**
 	 * This method first deletes all the files in given folder and than the
 	 * folder itself
 	 */
-	public static void deleteFolder(String bucketName, String folderName, AmazonS3 client) {
-		List<S3ObjectSummary> fileList = 
-				client.listObjects(bucketName, folderName).getObjectSummaries();
+	public static void deleteFolder(String bucketName, String folderName,
+			AmazonS3 client) {
+		List<S3ObjectSummary> fileList = client.listObjects(bucketName,
+				folderName).getObjectSummaries();
 		for (S3ObjectSummary file : fileList) {
 			client.deleteObject(bucketName, file.getKey());
 		}
--- theinventory/src/main/java/svinbass/theinventory/aws/AWSSQSHelper.java
+++ theinventory/src/main/java/svinbass/theinventory/aws/AWSSQSHelper.java
@@ -40,172 +40,184 @@ import com.amazonaws.services.sqs.model.SendMessageRequest;
  * This sample demonstrates how to make basic requests to Amazon SQS using the
  * AWS SDK for Java.
  *
- * Prerequisites: You must have a valid Amazon Web Services developer account, 
- * and be signed up to use Amazon SQS. For more information about Amazon SQS, 
+ * Prerequisites: You must have a valid Amazon Web Services developer account,
+ * and be signed up to use Amazon SQS. For more information about Amazon SQS,
  * see http://aws.amazon.com/sqs
  * 
  * Fill in your AWS access credentials in the provided credentials file
  * template, and be sure to move the file to the default location
  * (~/.aws/credentials) where the sample code loads the credentials from.
  * 
- * IMPORTANT: To avoid accidental leakage of your credentials, DO NOT 
- * keep the credentials file in your source directory.
+ * IMPORTANT: To avoid accidental leakage of your credentials, DO NOT keep the
+ * credentials file in your source directory.
  */
 public class AWSSQSHelper {
-	
-	 private static final String QUEUE_NAME = "vikiQueue" ;
-	 
-	 private static final Logger logger_c = Logger.getLogger(AWSSQSHelper.class);
 
-    public static void main(String[] args) throws Exception {
+	private static final String QUEUE_NAME = "vikiQueue";
 
-        /*
-         * The ProfileCredentialsProvider returns your [default]
-         * credential profile by reading from the credentials file located at
-         * (~/.aws/credentials).
-         */
-        AWSCredentials credentials = null;
-        try {
-            credentials = new ProfileCredentialsProvider().getCredentials();
-        } catch (Exception e) {
-            throw new AmazonClientException(
-                    "Cannot load the credentials from the credential profiles file. " +
-                    "Please make sure that your credentials file is at the correct " +
-                    "location (~/.aws/credentials), and is in valid format.",
-                    e);
-        }
+	private static final Logger logger_c = Logger.getLogger(AWSSQSHelper.class);
 
-        AmazonSQS sqs = AmazonSQSClientBuilder.standard()
-				.withRegion(Regions.US_EAST_1)
-				.build();
+	public static void main(String[] args) throws Exception {
 
-        System.out.println("===========================================");
-        System.out.println("Getting Started with Amazon SQS");
-        System.out.println("===========================================\n");
+		/*
+		 * The ProfileCredentialsProvider returns your [default] credential
+		 * profile by reading from the credentials file located at
+		 * (~/.aws/credentials).
+		 */
+		AWSCredentials credentials = null;
+		try {
+			credentials = new ProfileCredentialsProvider().getCredentials();
+		} catch (Exception e) {
+			throw new AmazonClientException(
+					"Cannot load the credentials from the credential profiles file. "
+							+ "Please make sure that your credentials file is at the correct "
+							+ "location (~/.aws/credentials), and is in valid format.",
+					e);
+		}
 
-        try {
-            // Create a queue
-            System.out.println("Creating a new SQS queue called MyQueue.\n");
-            CreateQueueRequest createQueueRequest = new CreateQueueRequest("MyQueue");
-            String myQueueUrl = sqs.createQueue(createQueueRequest).getQueueUrl();
+		AmazonSQS sqs = AmazonSQSClientBuilder.standard()
+				.withRegion(Regions.US_EAST_1).build();
 
-            // List queues
-            System.out.println("Listing all queues in your account.\n");
-            for (String queueUrl : sqs.listQueues().getQueueUrls()) {
-                System.out.println("  QueueUrl: " + queueUrl);
-            }
-            System.out.println();
+		System.out.println("===========================================");
+		System.out.println("Getting Started with Amazon SQS");
+		System.out.println("===========================================\n");
 
-            // Send a message
-            System.out.println("Sending a message to MyQueue.\n");
-            sqs.sendMessage(new SendMessageRequest(myQueueUrl, "This is my message text."));
+		try {
+			// Create a queue
+			System.out.println("Creating a new SQS queue called MyQueue.\n");
+			CreateQueueRequest createQueueRequest = new CreateQueueRequest(
+					"MyQueue");
+			String myQueueUrl = sqs.createQueue(createQueueRequest)
+					.getQueueUrl();
 
-            // Receive messages
-            System.out.println("Receiving messages from MyQueue.\n");
-            ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(myQueueUrl);
-            List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
-            for (Message message : messages) {
-                System.out.println("  Message");
-                System.out.println("    MessageId:     " + message.getMessageId());
-                System.out.println("    ReceiptHandle: " + message.getReceiptHandle());
-                System.out.println("    MD5OfBody:     " + message.getMD5OfBody());
-                System.out.println("    Body:          " + message.getBody());
-                for (Entry<String, String> entry : message.getAttributes().entrySet()) {
-                    System.out.println("  Attribute");
-                    System.out.println("    Name:  " + entry.getKey());
-                    System.out.println("    Value: " + entry.getValue());
-                }
-            }
-            System.out.println();
+			// List queues
+			System.out.println("Listing all queues in your account.\n");
+			for (String queueUrl : sqs.listQueues().getQueueUrls()) {
+				System.out.println("  QueueUrl: " + queueUrl);
+			}
+			System.out.println();
 
-            // Delete a message
-            System.out.println("Deleting a message.\n");
-            String messageReceiptHandle = messages.get(0).getReceiptHandle();
-            sqs.deleteMessage(new DeleteMessageRequest(myQueueUrl, messageReceiptHandle));
+			// Send a message
+			System.out.println("Sending a message to MyQueue.\n");
+			sqs.sendMessage(new SendMessageRequest(myQueueUrl,
+					"This is my message text."));
 
-            // Delete a queue
-            System.out.println("Deleting the test queue.\n");
-            sqs.deleteQueue(new DeleteQueueRequest(myQueueUrl));
-        } catch (AmazonServiceException ase) {
-            System.out.println("Caught an AmazonServiceException, which means your request made it " +
-                    "to Amazon SQS, but was rejected with an error response for some reason.");
-            System.out.println("Error Message:    " + ase.getMessage());
-            System.out.println("HTTP Status Code: " + ase.getStatusCode());
-            System.out.println("AWS Error Code:   " + ase.getErrorCode());
-            System.out.println("Error Type:       " + ase.getErrorType());
-            System.out.println("Request ID:       " + ase.getRequestId());
-        } catch (AmazonClientException ace) {
-            System.out.println("Caught an AmazonClientException, which means the client encountered " +
-                    "a serious internal problem while trying to communicate with SQS, such as not " +
-                    "being able to access the network.");
-            System.out.println("Error Message: " + ace.getMessage());
-        }
-    }
-    
-    
-    
-    public static void createQueueAndSendMessageToSQS(String message){
-    	final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+			// Receive messages
+			System.out.println("Receiving messages from MyQueue.\n");
+			ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(
+					myQueueUrl);
+			List<Message> messages = sqs.receiveMessage(receiveMessageRequest)
+					.getMessages();
+			for (Message message : messages) {
+				System.out.println("  Message");
+				System.out.println("    MessageId:     "
+						+ message.getMessageId());
+				System.out.println("    ReceiptHandle: "
+						+ message.getReceiptHandle());
+				System.out.println("    MD5OfBody:     "
+						+ message.getMD5OfBody());
+				System.out.println("    Body:          " + message.getBody());
+				for (Entry<String, String> entry : message.getAttributes()
+						.entrySet()) {
+					System.out.println("  Attribute");
+					System.out.println("    Name:  " + entry.getKey());
+					System.out.println("    Value: " + entry.getValue());
+				}
+			}
+			System.out.println();
 
-    	logger_c.info("createQueueAndSendMessageToSQS sqs fetched");
-        try {
-            CreateQueueResult create_result = sqs.createQueue(QUEUE_NAME);
-        	logger_c.info("createQueueAndSendMessageToSQS create_result");
-        } catch (AmazonSQSException e) {
-            if (!e.getErrorCode().equals("QueueAlreadyExists")) {
-                throw e;
-            }
-        }
-        
-        String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
-        logger_c.info("createQueueAndSendMessageToSQS queueUrl"+queueUrl);
-        
-        SendMessageRequest send_msg_request = new SendMessageRequest()
-        .withQueueUrl(queueUrl)
-        .withMessageBody(message)
-        .withDelaySeconds(5);
-    	sqs.sendMessage(send_msg_request);
-    	
-    	logger_c.info("createQueueAndSendMessageToSQS send_msg_request "+message);
-    }
-    
-    
-    public static void sendMessageToSQS(String message){
-    	final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
-    	logger_c.info("sendMessageToSQS sqs");
-    	String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
-    	logger_c.info("sendMessageToSQS queueUrl "+queueUrl);
-    	
-    	SendMessageRequest send_msg_request = new SendMessageRequest()
-        .withQueueUrl(queueUrl)
-        .withMessageBody(message)
-        .withDelaySeconds(5);
-    	sqs.sendMessage(send_msg_request);
-    	logger_c.info("sendMessageToSQS sendMessageToSQS "+message);
-    }
-    
-    public static List<String> receiveMessagesFromSQS(){
-    	final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
-    	logger_c.info("receiveMessagesFromSQS sqs");
-    	String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
-    	logger_c.info("receiveMessagesFromSQS queueUrl "+queueUrl);
-    	 List<Message> messages = sqs.receiveMessage(queueUrl).getMessages();
-    	 logger_c.info("receiveMessagesFromSQS messages size "+messages.size());
-         // delete messages from the queue
-         for (Message m : messages) {
-        	 logger_c.info("receiveMessagesFromSQS before deletion  "+m.getBody());
-             sqs.deleteMessage(queueUrl, m.getReceiptHandle());
-             logger_c.info("receiveMessagesFromSQS deleteMessage ");
-         }
-         
-         List<String> messageStrList = new ArrayList<String>();
-         
-         for (Message m : messages) {
-        	 messageStrList.add(m.getBody());
-        	 logger_c.info("receiveMessagesFromSQS message body "+m.getBody());
-         }
-         
-         logger_c.info("receiveMessagesFromSQS returning");
-         return messageStrList;
-    }
+			// Delete a message
+			System.out.println("Deleting a message.\n");
+			String messageReceiptHandle = messages.get(0).getReceiptHandle();
+			sqs.deleteMessage(new DeleteMessageRequest(myQueueUrl,
+					messageReceiptHandle));
+
+			// Delete a queue
+			System.out.println("Deleting the test queue.\n");
+			sqs.deleteQueue(new DeleteQueueRequest(myQueueUrl));
+		} catch (AmazonServiceException ase) {
+			System.out
+					.println("Caught an AmazonServiceException, which means your request made it "
+							+ "to Amazon SQS, but was rejected with an error response for some reason.");
+			System.out.println("Error Message:    " + ase.getMessage());
+			System.out.println("HTTP Status Code: " + ase.getStatusCode());
+			System.out.println("AWS Error Code:   " + ase.getErrorCode());
+			System.out.println("Error Type:       " + ase.getErrorType());
+			System.out.println("Request ID:       " + ase.getRequestId());
+		} catch (AmazonClientException ace) {
+			System.out
+					.println("Caught an AmazonClientException, which means the client encountered "
+							+ "a serious internal problem while trying to communicate with SQS, such as not "
+							+ "being able to access the network.");
+			System.out.println("Error Message: " + ace.getMessage());
+		}
+	}
+
+	public static void createQueueAndSendMessageToSQS(String message) {
+		final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+
+		logger_c.info("createQueueAndSendMessageToSQS sqs fetched");
+		try {
+			CreateQueueResult create_result = sqs.createQueue(QUEUE_NAME);
+			logger_c.info("createQueueAndSendMessageToSQS create_result");
+		} catch (AmazonSQSException e) {
+			if (!e.getErrorCode().equals("QueueAlreadyExists")) {
+				throw e;
+			}
+		}
+
+		String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
+		logger_c.info("createQueueAndSendMessageToSQS queueUrl" + queueUrl);
+
+		SendMessageRequest send_msg_request = new SendMessageRequest()
+				.withQueueUrl(queueUrl).withMessageBody(message)
+				.withDelaySeconds(5);
+		sqs.sendMessage(send_msg_request);
+
+		logger_c.info("createQueueAndSendMessageToSQS send_msg_request "
+				+ message);
+	}
+
+	public static void sendMessageToSQS(String message) {
+		final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+		logger_c.info("sendMessageToSQS sqs");
+		String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
+		logger_c.info("sendMessageToSQS queueUrl " + queueUrl);
+
+		SendMessageRequest send_msg_request = new SendMessageRequest()
+				.withQueueUrl(queueUrl).withMessageBody(message)
+				.withDelaySeconds(5);
+		sqs.sendMessage(send_msg_request);
+		logger_c.info("sendMessageToSQS sendMessageToSQS " + message);
+	}
+
+	public static List<String> receiveMessagesFromSQS() {
+		final AmazonSQS sqs = AmazonSQSClientBuilder.defaultClient();
+		logger_c.info("receiveMessagesFromSQS sqs");
+		String queueUrl = sqs.getQueueUrl(QUEUE_NAME).getQueueUrl();
+		logger_c.info("receiveMessagesFromSQS queueUrl " + queueUrl);
+		ReceiveMessageRequest receiveMessageRequest = new ReceiveMessageRequest(queueUrl).withAttributeNames("All");
+		
+		receiveMessageRequest.setMaxNumberOfMessages(10);
+
+		List<Message> messages = sqs.receiveMessage(receiveMessageRequest).getMessages();
+		logger_c.info("receiveMessagesFromSQS messages size " + messages.size());
+		// delete messages from the queue
+		for (Message m : messages) {
+			logger_c.info("receiveMessagesFromSQS before deletion  "
+					+ m.getBody());
+			sqs.deleteMessage(queueUrl, m.getReceiptHandle());
+			logger_c.info("receiveMessagesFromSQS deleteMessage ");
+		}
+
+		List<String> messageStrList = new ArrayList<String>();
+
+		for (Message m : messages) {
+			messageStrList.add(m.getBody());
+			logger_c.info("receiveMessagesFromSQS message body " + m.getBody());
+		}
+
+		logger_c.info("receiveMessagesFromSQS returning");
+		return messageStrList;
+	}
 }
